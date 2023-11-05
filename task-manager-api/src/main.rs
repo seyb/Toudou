c5acf312a01d69e36b35c39992d58dd231e4edcd
Sebastien PENCREACH <1299512+seyb@users.noreply.github.com>
2023-11-05 21:54:07 +0100

    Add serialization capability to Task manager
    The ability to serialize and deserialize our Task and TaskCollection structures was introduced. The `serde` library was added as an optional dependency to the task manager and serialize feature was implemented in 'lib.rs'. Now, fields in Task and TaskCollection are public to make them accessible for serialization. Task::new() method now takes a string slice (&str) for more convenient usage. Some test adjustments were made due to these changes. Task manager API now has the serialize feature enabled in its dependencies and contains a new route for getting tasks, serving serialized task data.

--- task-manager-api/src/main.rs
+++ task-manager-api/src/main.rs
@@ -1,7 +1,7 @@
-use axum::{routing::get, Router, Json};
 use axum::http::StatusCode;
-use axum::routing::post;
-use serde::{Deserialize, Serialize};
+use axum::{routing::get, Json, Router};
+
+use seybio_task_manager::{Collection, Task, TaskCollection};
 
 #[tokio::main]
 async fn main() {
@@ -11,9 +11,8 @@ async fn main() {
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
-        .route("/", get(root))
-        // `POST /users` goes to `create_user`
-        .route("/users", post(create_user));
+        .route("/health", get(root))
+        .route("/tasks", get(get_tasks));
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3001".parse().unwrap())
         .serve(app.into_make_service())
@@ -23,34 +22,12 @@ async fn main() {
 
 // basic handler that responds with a static string
 async fn root() -> &'static str {
-    "Hello, World from root !"
+    "it works"
 }
 
-async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-    // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
-}
-
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-// the output to our `create_user` handler
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
+async fn get_tasks() -> (StatusCode, Json<Vec<Task>>) {
+    let mut collection = TaskCollection::new();
+    let task = Task::new("test");
+    collection.add_task(task);
+    (StatusCode::OK, Json(collection.tasks))
 }
--- task-manager/src/lib.rs
+++ task-manager/src/lib.rs
@@ -1,17 +1,17 @@
 use std::time::SystemTime;
 
-#[derive(PartialEq)]
+#[cfg_attr(feature = "serialize", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Task {
-    title: String,
-    description: String,
-    completed_at: Option<SystemTime>,
+    pub title: String,
+    pub description: String,
+    pub completed_at: Option<SystemTime>,
 }
 
-
 impl Task {
-    pub fn new(title: String) -> Self {
+    pub fn new(title: &str) -> Self {
         Self {
-            title,
+            title: title.to_string(),
             description: "".to_string(),
             completed_at: None,
         }
@@ -20,7 +20,7 @@ impl Task {
     pub fn complete(&mut self) {
         match self.completed_at {
             None => self.completed_at = Some(SystemTime::now()),
-            x => self.completed_at = x
+            x => self.completed_at = x,
         }
     }
     pub fn uncomplete(&mut self) {
@@ -28,37 +28,30 @@ impl Task {
     }
 }
 
-
 pub trait Collection {
     type Task;
-    fn new () -> Self;
+    fn new() -> Self;
     fn add_task(&mut self, task: Self::Task);
     fn remove_task(&mut self, task: Self::Task);
-    fn get_all_tasks(&self) -> &Vec<Self::Task>;
 }
 
+#[cfg_attr(feature = "serialize", derive(serde::Serialize, serde::Deserialize))]
 pub struct TaskCollection {
-    tasks: Vec<Task>,
+    pub tasks: Vec<Task>,
 }
 
 impl Collection for TaskCollection {
     type Task = Task;
 
     fn new() -> Self {
-        Self {
-            tasks: vec![]
-        }
+        Self { tasks: vec![] }
     }
     fn add_task(&mut self, task: Self::Task) {
         self.tasks.push(task);
     }
 
-    fn remove_task(&mut self, task: Self::Task){
+    fn remove_task(&mut self, task: Self::Task) {
         self.tasks.retain(|t| *t != task);
-    }
-
-    fn get_all_tasks(&self) -> &Vec<Self::Task> {
-        &self.tasks
     }
 }
 
@@ -68,25 +61,25 @@ mod tests {
 
     #[test]
     fn it_inits_task() {
-        let expected_title = String::from("new Task");
-        let task = Task::new(expected_title.clone());
-        assert_eq!(task.title, expected_title);
+        let expected_title = "new Task";
+        let task = Task::new(expected_title);
+        assert_eq!(task.title, expected_title.to_string());
         assert_eq!(task.description, "".to_string());
         assert_eq!(task.completed_at, None)
     }
 
     #[test]
     fn it_completes_a_task() {
-        let expected_title = String::from("new Task");
-        let mut task = Task::new(expected_title.clone());
+        let expected_title = "new Task";
+        let mut task = Task::new(expected_title);
         task.complete();
         assert_ne!(task.completed_at, None)
     }
 
     #[test]
     fn it_uncompletes_a_task() {
-        let expected_title = String::from("new Task");
-        let mut task = Task::new(expected_title.clone());
+        let expected_title = "new Task";
+        let mut task = Task::new(expected_title);
         task.complete();
         task.uncomplete();
         assert_eq!(task.completed_at, None)
@@ -94,8 +87,8 @@ mod tests {
 
     #[test]
     fn it_does_not_set_completes_mulitple_times() {
-        let expected_title = String::from("new Task");
-        let mut task = Task::new(expected_title.clone());
+        let expected_title = "new Task";
+        let mut task = Task::new(expected_title);
         task.complete();
         let expected_completed = task.completed_at;
         task.complete();
@@ -105,6 +98,15 @@ mod tests {
     #[test]
     fn inits_empty_task_collection() {
         let collection = TaskCollection::new();
-        assert_eq!(collection.tasks.is_empty(), true)
+        assert!(collection.tasks.is_empty())
+    }
+
+    #[test]
+    fn test_get_all_tasks_from_collection() {
+        let mut collection = TaskCollection::new();
+        let task1 = Task::new("task 1");
+
+        collection.add_task(task1.clone());
+        assert_eq!(collection.tasks, vec![task1])
     }
 }
